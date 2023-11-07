c83dc95cbc504d16350393758ce857cf7a77d19b
Leonardo-Luz <leonardoluz07@gmail.com>
2023-11-07 18:35:16 -0300

    [feat] navbar update

--- packages/client/src/components/navbar/Navbar.tsx
+++ packages/client/src/components/navbar/Navbar.tsx
@@ -1,20 +1,38 @@
 import '../../styles/navbar.style.css'
 import NavbarLink from './NavbarElements';
-import profile from '../../images/profile2.png'
+import profile from '../../images/profile.png'
+import { useState } from 'react';
 
 const Navbar = () => 
 {
+
+    const [clicked , setClicked] = useState<boolean>(false);
+
+    const clickHandle = () => 
+    {
+        setClicked(!clicked);
+    }
+
+
     return(
         <div id='navbar'>
+            {<input type="checkbox" id='hamburger-input' readOnly checked={clicked} />}
             <div id="inside">
-                <div id="hamburger-menu">
+                <label id="hamburger-menu" onClick={() => {clickHandle()}} />
+
+                <div id='links' onClick={() => {clickHandle()}}>
+                    <p className='menu-button'>Menu</p>
+                    <NavbarLink href='/'>Home</NavbarLink>
+                    <NavbarLink href='/'>Adicionar Proposta</NavbarLink>
+                    <NavbarLink href='/'>Minhas Propostas</NavbarLink>
                 </div>
 
                 <div id="Links">
-                    <NavbarLink href='/'>Home</NavbarLink>
-                    <NavbarLink href='/profile'><img src={profile} alt='Profile'/></NavbarLink>
                 </div>
+                <NavbarLink href='/profile'><img src={profile} alt='Profile'/></NavbarLink>
             </div>
+
+            <div className="overlay" onClick={() => {clickHandle()}}></div>      
         </div>
     )
 }
