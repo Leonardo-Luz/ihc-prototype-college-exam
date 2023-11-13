f4956674215504855a8440a2761ace201db4f480
Leonardo-Luz <leonardoluz07@gmail.com>
2023-11-13 18:59:33 -0300

    [wip] add product list

--- packages/client/src/routes/Product-List.tsx
+++ packages/client/src/routes/Product-List.tsx
@@ -1,9 +1,60 @@
-const ProductList = () => {
-        
+import { InMemoryProduct } from "../databaseInMem/controllers/product.controller";
+import { productModel } from "../databaseInMem/models";
+
+type ProductRowProps = {
+    product: productModel
+}
+
+const ProductRow = ( { product }: ProductRowProps ) => {
     return (
-        <div>
-            Cenoura
-        </div>
+        <tr>
+            <td>{product.productId}</td>
+            <td>{product.createdAt}</td>
+            <td>{product.updatedAt}</td>
+        </tr>
+    )
+}
+
+const ProductHead = () => {
+    return (
+        <thead>
+            <td>ID</td>
+            <td>createdAt</td>
+            <td>updatedAt</td>
+        </thead>
+    )
+}
+
+const ProductList = () => {
+    const product = new InMemoryProduct();
+
+    product.inMemCreateProduct({
+        category: 'produto',
+        description: 'aaaaa',
+        name: 'bola',
+        productId: product.itens.length + 1,
+        stock: 3,
+    })
+
+    product.inMemCreateProduct({
+        category: 'produto',
+        description: 'aaaaa',
+        name: 'bola',
+        productId: product.itens.length + 1,
+        stock: 3,
+    })    
+
+    return (
+        <table>
+            <ProductHead/>
+            <tbody>
+                {
+                    product.itens && product.itens.map( (data) => {
+                        return <ProductRow product={data} /> 
+                    })
+                }
+            </tbody>
+        </table>
     )
 }
 
