e397eb0274f23ba47025ee08af4ee4fd9f11a8fc
Leonardo-Luz <leonardoluz07@gmail.com>
2023-11-21 18:04:15 -0300

    [feat] add select product row

---
+++ packages/client/src/components/row/ProductSelectRow.tsx
@@ -0,0 +1,29 @@
+import { productModel } from "../../databaseInMem/models";
+
+type ProductRowProps = {
+    product: productModel,
+    clickHandle: () => void
+}
+ 
+const ProductSelectRow = ( { product , clickHandle }: ProductRowProps ) => {
+
+    return (
+        <div className="product-list-container" 
+            onClick={clickHandle}
+        >
+        {
+            product.image &&
+            <div className="product-img-holder">
+                <img src={URL.createObjectURL(product.image)} alt={product.name} />
+            </div>
+        }
+            <div className="product-attributes">
+                <p>{product.category}: {product.name}</p>
+                <p>Estoque: {product.stock}</p>
+                <p>Categoria: {product.category}</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProductSelectRow;
