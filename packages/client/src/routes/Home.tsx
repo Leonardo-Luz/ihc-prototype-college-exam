daa5b8dbdebd42454656c313f0df5cbe07361e8b
Leonardo-Luz <leonardoluz07@gmail.com>
2023-11-21 18:05:53 -0300

    [wip] home page

--- packages/client/src/routes/Home.tsx
+++ packages/client/src/routes/Home.tsx
@@ -1,8 +1,25 @@
+import ProductRow from "../components/row/ProductRow";
+import { product } from "../databaseInMem/controllers/product.controller";
+
+import '../styles/product-container.style.css'
+
 
 const Home = () => {
     return (
-        <div>
+        <div className="body">
             Home
+            <div className="product-list">
+            {
+                (
+                    product.itens.length === 0 && <tr><td>No Data</td></tr>
+                ) ||
+                (
+                    product.itens && product.itens.map( (data) => {
+                        return <ProductRow product={data} /> 
+                    }) 
+                )
+            }
+            </div>
         </div>
     )
 }
