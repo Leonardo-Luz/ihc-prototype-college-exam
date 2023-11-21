952d91775084695f2451e8155f40d0d83240b623
Leonardo-Luz <leonardoluz07@gmail.com>
2023-11-21 18:10:50 -0300

    [feat] table removed

--- packages/client/src/routes/Product-List.tsx
+++ packages/client/src/routes/Product-List.tsx
@@ -1,56 +1,20 @@
-import '../styles/table.style.css'
-
 import { product } from "../databaseInMem/controllers/product.controller";
-import { productModel } from "../databaseInMem/models";
 
-type ProductRowProps = {
-    product: productModel
-}
- 
-const ProductRow = ( { product }: ProductRowProps ) => {
-    return (
-        <tr className='row'>
-            <td className='col' >{product.productId}</td>
-            <td className='col' >{product.name}</td>
-            <td className='col' >{product.description}</td>
-            <td className='col' >{product.stock}</td>
-            <td className='col' >{product.category}</td>
-        </tr>
-    )
-}
-
-const ProductHead = () => {
-    return (
-        <thead className='thead'>
-            <tr className='row'>
-                <td className='col' >ID</td>
-                <td className='col' >Nome</td>
-                <td className='col' >Descrição</td>
-                <td className='col' >Estoque</td>
-                <td className='col' >Categoria</td>
-            </tr>
-        </thead>
-    )
-}
+import ProductRow from '../components/row/ProductRow';
 
 const ProductList = () => {
     return (
         <div className="body">
-            <table className='table'>
-                <ProductHead/>
-                <tbody className='tbody'>
-                    {
-                        (
-                            product.itens.length === 0 && <tr><td>No Data</td></tr>
-                        ) ||
-                        (
-                            product.itens && product.itens.map( (data) => {
-                                return <ProductRow product={data} /> 
-                            }) 
-                        )
-                    }
-                </tbody>
-            </table>
+            {
+                (
+                    product.itens.length === 0 && <tr><td>No Data</td></tr>
+                ) ||
+                (
+                    product.itens && product.itens.map( (data) => {
+                        return <ProductRow product={data} /> 
+                    }) 
+                )
+            }
         </div>
     )
 }
